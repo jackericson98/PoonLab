10defd9cf019647b0aeee819e929b56aeeadcfbc
i5-2500 <jackericson98@gmail.com>
2021-09-24 16:13:18 -0400

    worked on NMR animation

---
+++ NMR/NMR_animation.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
+
+speed = 2
+arrow_length = 1
+theta_real = 3 * np.pi / 8
+radius = arrow_length * np.sin(theta_real)
+n = 100 // speed  # NUMBER OF FRAMES
+
+
+def get_arrow(phi):
+    x = 0
+    y = 0
+    z = 0
+    u = radius * np.sin(phi)
+    v = radius * np.cos(phi)
+    w = 0.5 * arrow_length
+    return x, y, z, u, v, w
+
+
+def rotation_x(angle):
+    Rx = [[1, 0, 0], [0, np.cos(angle), -np.sin(angle)], [0, np.sin(angle), np.cos(angle)]]
+    return np.array(Rx)
+
+
+def rotation_z(angle):
+    Rz = [[np.cos(angle), -np.sin(angle), 0], [np.sin(angle), np.cos(angle), 0], [0, 0, 1]]
+    return np.array(Rz)
+
+
+# Draw Sphere
+def get_sphere():
+    theta_construct = np.linspace(0, 2*np.pi, 20)
+    phi_construct = np.linspace(0, np.pi, 10)
+    phi_construct, theta_construct = np.meshgrid(phi_construct, theta_construct)
+    x = 0.5 * radius * np.cos(theta_construct) * np.sin(phi_construct)
+    y = 0.5 * radius * np.sin(theta_construct) * np.sin(phi_construct)
+    z = 0.5 * radius * np.cos(phi_construct)
+    return x, y, z
+
+
+def rotate_sphere(angle):
+    x = []
+    y = []
+    z = []
+    for i in range(len(get_sphere()[0][0])):
+        for j in range(len(get_sphere()[0][1])):
+            point = [get_sphere()[0][i][j], get_sphere()[1][i][j], get_sphere()[2][i][j]]
+            rotated_point = rotation_x(angle).dot(point)
+            x.append(rotated_point[0])
+            y.append(rotated_point[1])
+            z.append(rotated_point[2])
+    return x, y, z
+
+
+sphere = ax.plot_surface(*rotate_sphere(np.pi/4))
+quiver = ax.quiver(*get_arrow(0), normalize=True, pivot='tail', capstyle='butt')
+
+ax.set_xlim(-2, 2)
+ax.set_ylim(-2, 2)
+ax.set_zlim(-2, 2)
+
+
+def update(phi):
+    global quiver
+    quiver.remove()
+    quiver = ax.quiver(*get_arrow(phi))
+
+    # global sphere
+    # sphere.remove()
+    # sphere = ax.plot_surface(x, y, z, color="r")
+
+
+# Plot the animated rotating arrow
+ani = FuncAnimation(fig, update, frames=np.linspace(0, 2 * np.pi, n), interval=0.0005)
+plt.show()
--- NMR/linear_best_fit.py
+++ NMR/linear_best_fit.py
@@ -1,43 +1,47 @@
-
 """This function creates random error exponential decay functions"""
 import random
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as optimize
+from statsmodels.regression.quantile_regression import QuantReg
 
 
-def rand_error(datapoint, percent):
+def rand_error(datapoint):
     err_max = datapoint
-    data_error = 2*(random.random()-0.5)*err_max
+    data_error = 2 * (random.random() - 0.5) * err_max
     return data_error
 
 
 def error_data(old_data, percent):
     new_data = []
     for data_point in old_data:
-        new_data_point = rand_error(data_point, percent)
+        new_data_point = data_point + rand_error(data_point) * percent
         new_data.append(new_data_point)
     return new_data
 
 
-def func(x, m, b):
-    y = m*x + b
+def lin_func(x, m, b):
+    y = m * x + b
     return y
+
+
+def exp_func(x, a, b):
+    y = a * np.exp(b * x)
+    return y
+
 
 num_points = 100
 
-xdata = np.linspace(0, 100, num_points)
-ydata = error_data(xdata, 0.15)
-popt, pcov = optimize.curve_fit(func, xdata, ydata)
+x_data = np.linspace(0, 100, num_points)
+y_data = exp_func(x_data, 2, 4)
+lin_data, p_cov = optimize.curve_fit(exp_func, x_data, y_data)
 
-print(popt)
 
-slope = popt[0]
-intercept = popt[1]
-new_ydata = xdata*slope
+slope = lin_data[0]
+intercept = lin_data[1]
+new_y_data = x_data * slope
 
-plt.scatter(xdata, ydata)
-plt.plot(xdata, new_ydata)
+plt.scatter(x_data, y_data, )
+plt.plot(x_data, new_y_data)
 
 plt.show()
-

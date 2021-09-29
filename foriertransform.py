f0afbc5883c68f0b59241d37484147f70390ad04
i5-2500 <jackericson98@gmail.com>
2021-09-29 16:17:17 -0400

    fourier transform file adjusted

--- foriertransform.py
+++ foriertransform.py
@@ -1,8 +1,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.fft import fft, fftfreq
 
 SAMPLE_RATE = 44100  # Hertz
 DURATION = 5  # Seconds
+
 
 def generate_sine_wave(freq, sample_rate, duration):
     x = np.linspace(0, duration, sample_rate * duration, endpoint=False)
@@ -10,6 +12,7 @@ def generate_sine_wave(freq, sample_rate, duration):
     # 2pi because np.sin takes radians
     y = np.sin((2 * np.pi) * frequencies)
     return x, y
+
 
 _, nice_tone = generate_sine_wave(400, SAMPLE_RATE, DURATION)
 _, noise_tone = generate_sine_wave(4000, SAMPLE_RATE, DURATION)
@@ -22,8 +25,6 @@ normalized_tone = np.int16((mixed_tone / mixed_tone.max()) * 32767)
 plt.plot(normalized_tone[:1000])
 plt.show()
 
-from scipy.fft import fft, fftfreq
-
 # Number of samples in normalized_tone
 N = SAMPLE_RATE * DURATION
 
@@ -32,5 +33,3 @@ xf = fftfreq(N, 1 / SAMPLE_RATE)
 
 plt.plot(xf, np.abs(yf))
 plt.show()
-
-nmrsim.qm()

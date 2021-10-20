837344d70ad67f8184ed9d6bb46c757d9284c5e1
i5-2500 <jackericson98@gmail.com>
2021-10-20 12:17:23 -0400

    Made a class for test data Fixed noise function issue Added plot info

--- NMR/single_relaxation_plot.py
+++ NMR/single_relaxation_plot.py
@@ -1,26 +1,74 @@
-from functions import *
 import matplotlib.pyplot as plt
 import numpy as np
-from print2data import *
 from scipy.optimize import curve_fit
 
+"""This file creates an exponential decay function, adds random noise and then fits a line of best fit to the noise. 
+Will eventually be repurposed to be applied to NMR relaxation data to return values for T1 and T2, which can then be 
+plotted and shown as an animation in full_spectrum_relaxation_animation.py"""
 
-def make_y_data(decay_constant, num_points):
-    y = []
-    for i in range(num_points):
-        y.append((decay_at_time([vol[0]], i, decay_constant))[0])
+
+class Test_data:
+
+    """Creates an exponential decay data set with random noise"""
+
+    def __init__(self, x_test=np.linspace(0.1, 1.1, 100), decay_constant=-10, noise=0.05):
+        self.decay_constant = decay_constant
+        self.x_test = x_test
+        self.y_test = []
+        self.noise = noise
+
+    def make_y_data(self):
+
+        """Make decay data from given an x range and and a decay constant"""
+
+        decay_constant = self.decay_constant
+        x_test = self.x_test
+        self.y_test = np.exp(x_test * decay_constant)
+        return self.y_test
+
+    def make_noise_data(self):
+
+        """For each value in our test data array add a random amount of noise given by noise var"""
+
+        input_data = self.make_y_data()
+        noise = self.noise
+        noise_array = np.abs(np.random.normal(0, noise, len(input_data)))
+        self.y_test = np.add(input_data, noise_array)
+        return self.y_test
+
+    def __str__(self):
+        return str(self.y_test)
+
+# ----------------------------- Curve Fitting ---------------------------
+
+
+# Create an exponential function
+def exp_func(x, a, b, c):
+    y = a * np.exp(b * x) + c
     return y
 
 
-num_its = 100
-x_data = np.linspace(0.1, 1.1, num_its)
-#y_data = add_noise(make_y_data(.08, len(x_data)))
-y_data = add_noise(3.4 * np.exp(-2 * x_data) + 4)
-best_fit, _ = curve_fit(exp_func, x_data, y_data, maxfev=5000)
-a, b, c = best_fit[0], best_fit[1], best_fit[2]
-y_fit = a * np.exp(x_data * b) + c
+# Create a curve fitting function
+def exp_fit(x_vals, y_vals):
+    best_fit, _ = curve_fit(exp_func, x_vals, y_vals, p0=(0, 0, 0), maxfev=500000)
+    a, b, c = best_fit[0], best_fit[1], best_fit[2]
+    y_fit = a * np.exp(x_vals * b) + c
+    return y_fit, a, b, c
+
+
+# Create our test data sets
+x_data = np.linspace(0.1, 1.1, 100)
+y_data = Test_data(x_data).make_noise_data()
+y_fitted = exp_fit(x_data, y_data)
+
+# ------------------- Plotting ---------------------
+
 ax = plt.gca()
-ax.text(0.5, 0.75, "Relaxation Constant = %.2f" % b, horizontalalignment='left', verticalalignment='top', transform=ax.transAxes)
-plt.scatter(x_data, y_data)
-plt.plot(x_data, y_fit)
+plt.title("Best fit for exponential decay data")
+plt.xlabel("Time")
+plt.ylabel("Data")
+ax.text(0.5, 0.75, "Relaxation Constant = %.2f" % y_fitted[2],
+        horizontalalignment='left', verticalalignment='top', transform=ax.transAxes)
+plt.scatter(x_data, y_data, color='blue')  # Noisy data
+plt.plot(x_data, y_fitted[0], color='red')  # Fitted data
 plt.show()

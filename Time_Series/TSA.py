4d571a8f90c688b079d172815c3bc83f88cd3f9a
i5-2500 <jackericson98@gmail.com>
2021-10-20 12:20:06 -0400

    Created

---
+++ Time_Series/TSA.py
@@ -0,0 +1,23 @@
+from dateutil.parser import parse
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import pandas as pd
+#plt.rcParams.update({'figure.figsize': (10, 7), 'figure.dpi': 120})
+from statsmodels.tsa.seasonal import seasonal_decompose
+from dateutil.parser import parse
+
+# Import Data
+df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'], index_col='date')
+
+# Multiplicative Decomposition
+result_mul = seasonal_decompose(df['value'], model='multiplicative', extrapolate_trend='freq')
+
+# Additive Decomposition
+result_add = seasonal_decompose(df['value'], model='additive', extrapolate_trend='freq')
+
+# Plot
+df_reconstructed = pd.concat([result_mul.seasonal, result_mul.trend, result_mul.resid, result_mul.observed], axis=1)
+df_reconstructed.columns = ['seas', 'trend', 'resid', 'actual_values']
+df_reconstructed.head()

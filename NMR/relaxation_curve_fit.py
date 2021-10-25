d95e1a264857c8ae156e2f29c761b22d256a6571
i5-2500 <jackericson98@gmail.com>
2021-10-25 15:38:07 -0400

    fully integrated full_spectrum_relaxation_animation.py and finished test data class

--- NMR/relaxation_curve_fit.py
+++ NMR/relaxation_curve_fit.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import random as rd
@@ -9,55 +10,53 @@ from matplotlib.animation import FuncAnimation
 residue. The Test_data class can be used to produce test data for fitting and animation"""
 
 
+# ------------------------------- Test Data ---------------------------------------
 class Test_data:
-
     """Creates an exponential decay data set with random noise"""
 
-    def __init__(self, x_max=1, num_frames=32, decay_constant=[-10], noise=0.05, y0=1, vol_data=np.linspace(1, 1, 100)):
+    def __init__(self, x_max=1, num_frames=32, decay_constant=-10, noise=0.05, y0=1):
         self.decay_constant = decay_constant
         self.x_test = np.linspace(.01, x_max, num_frames)
         self.y_test = []
         self.noise = noise
         self.y0 = y0
         self.df = []
-        self.vol_data = vol_data
 
-    def make_y_data(self, y0):
-
+    def make_y_data(self, y0, decay_constant):
         """Make decay data from given an x range and and a decay constant"""
 
-        decay_constant = self.decay_constant
         x_test = self.x_test
         self.y_test = y0 * np.exp(x_test * decay_constant)
         return self.y_test
 
-    def make_noise_data(self, y0):
-
+    def make_noise_data(self, y0, decay_constant):
         """For each value in our test data array add a random amount of noise given by noise var"""
 
-        input_data = self.make_y_data(y0)
+        input_data = self.make_y_data(y0, decay_constant)
         noise = self.noise
         noise_array = np.abs(np.random.normal(0, noise, len(input_data)))
         self.y_test = np.add(input_data, noise_array)
         return self.y_test
 
-    def make_data_frame(self):  # Bug where this only returns the last array of the data frame
+    def make_data_frame(self, vol_data=np.linspace(1, 1, 100)):
 
-        """returns a data frame of test values each with unique relaxation constants"""
-        for peak in self.vol_data:
-            self.decay_constant = -10 * rd.random()
-            decay_storage.append(self.decay_constant)
-            ds = self.make_noise_data(peak)
+        """Creates a data frame of test values each with unique relaxation constants"""
+
+        decay_storage = []
+
+        for peak in vol_data:
+            decay_constant = -10 * rd.random()
+            decay_storage.append(decay_constant)
+            ds = self.make_noise_data(peak, decay_constant)
             self.df.append(ds)
-
-        jint = self.df
-        return print(jint)
+        print(decay_storage)
+        return self.df
 
     def __str__(self):
         return str(self.y_test)
 
-# ----------------------------- Curve Fitting ---------------------------
 
+# ----------------------------- Curve Fitting ---------------------------
 
 # Create an exponential function
 def exp_func(x, a, b, c):
@@ -66,75 +65,78 @@ def exp_func(x, a, b, c):
 
 
 # Create a curve fitting function
-def exp_fit(x_vals, y_vals):
-    a_guess = np.mean(y_vals)
-    best_fit, _ = curve_fit(exp_func, x_vals, y_vals, p0=(a_guess, 0, 0), maxfev=500000)
+def exp_fit(x_values, y_values):
+
+    """Single exponential fit function"""
+
+    best_fit, _ = curve_fit(exp_func, x_values, y_values, p0=(1e+9, -5, 0), maxfev=500)
     a, b, c = best_fit[0], best_fit[1], best_fit[2]
-    y_fit = a * np.exp(x_vals * b) + c
+    y_fit = a * np.exp(x_values * b) + c
     return y_fit, a, b, c
 
 
 def multi_exp_fit(vol_data, x_max):
-    """Takes a set of sets of data (ex: data = [dataset 1, ... , dataset n], dataset1 = [(1 x num steps) volume data]
-    and returns an array of relaxation rates for each peak"""
+
+    """Takes a full spectrum of relaxation data and returns an array of relaxation rates for each peak"""
+
     x_array = np.linspace(0, x_max, len(vol_data[0]))  # Array of time stamps for relaxation fitting
-    relaxation_data = []
+    a_array = []
+    b_array = []
+    c_array = []
+
     for point in vol_data:  # For each data point record relaxation constant
-        cum = exp_fit(x_array, point)[2]
-        relaxation_data.append(cum)
-    return relaxation_data
+
+        decay_fit = exp_fit(x_array, point)
+        a_array.append(decay_fit[1])
+        b_array.append(decay_fit[2])
+        c_array.append(decay_fit[3])
+
+    return b_array
 
 
-# # create datasets
-df = []
-decay_storage = []
-y0_array = vol
-x_end = 2
+# ------------------------------ Make our test data -------------------------
 
-for i in y0_array:
-    decay = -10 * rd.random()
-    decay_storage.append(decay)
-    ds = Test_data(x_max=x_end, decay_constant=decay, y0=i)
-    ds1 = ds.make_noise_data(i)
-    df.append(ds1)
-print(df[1])
-relax_vals = multi_exp_fit(df, x_end)
+df = Test_data(x_max=5).make_data_frame(vol)  # This would be the data we import
 
-# ------------------- Plotting ---------------------
+fits = multi_exp_fit(df, 5)  # Get an array of fits
+
+
+# ------------------------------ Plotting ------------------------------------
 fig, ax = plt.subplots(subplot_kw=dict(projection="3d"))
 
 
-vol_data = ax.scatter(w1, w2, vol)
+vol_plot = ax.scatter(w1, w2, vol, color='blue')
 
 
 def update(frame):
-
-    global vol_data, df
-    vol_data.remove()
-    print(frame)
-    ax.scatter(w1, w2, df[:][frame:frame+1])
+    global vol_plot, df
+    vol_plot.remove()
+    new_df = []
+    for element in df:
+        new_df.append(element[frame])
+    vol_plot = ax.scatter(w1, w2, new_df, color='blue')
 
 
 ani = FuncAnimation(fig, update, frames=32)
 
-
+ax.set_xlabel('Hydrogen Chemical Shift (ppm)')
+ax.set_ylabel('Nitrogen Chemical Shift (ppm)')
+ax.set_zlabel('Intensity')
+ax.set_title('HSQC Relaxation')
+z_max = np.max(vol)
+ax.set_zlim(0, z_max)
 plt.show()
 
 
-
-
-
-
-
 # Create single test data sets
-
-# y = Test_data()
-# y_data = y.make_noise_data()
+#
+# y = Test_data(decay_constant=-1.55)
+# y_data = y.make_noise_data(vol[0])
 # x_data = y.x_test
 # y_fitted = exp_fit(x_data, y_data)
-
-# Plot single data set against it's best fit line
-
+#
+# # Plot single data set against it's best fit line
+#
 # ax = plt.gca()
 # plt.title("Best fit for exponential decay data")
 # plt.xlabel("Time")
@@ -144,6 +146,3 @@ plt.show()
 # plt.scatter(x_data, y_data, color='blue')  # Noisy data
 # plt.plot(x_data, y_fitted[0], color='red')  # Fitted data
 # plt.show()
-
-
-

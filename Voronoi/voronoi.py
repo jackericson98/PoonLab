c7da9cf863c2004ff06bfb631b5cdc5cf84a4484
jackericson98 <jackericson98@gmail.com>
2022-04-18 01:26:13 -0400

    added more

--- Voronoi/voronoi.py
+++ Voronoi/voronoi.py
@@ -33,32 +33,49 @@ class Mesh:
 
     def build_grid(self):
         self.find_neighbors()
+        # self.build_polyhedron()
+
+    def build_polyhedron(self, atom):
+        for i in range(len(atom.edges)):
+            for j in range(i, len(atom.edges)):
+                pass
+
 
     # Method to build neighbor list for each atom
     def find_neighbors(self):
-        # Go through each atom in our atom list
+
+        # Go through each atom in our atom list and add edges
         for i in range(len(self.atom_list)):
+
             # Set atom variable
             atom = self.atom_list[i]
+
             # Go through all other atoms
             for j in range(len(self.atom_list)):
+
                 # set neighbor variable
                 neighbor = self.atom_list[j]
+
                 # Get the distance between atom and the neighbor
                 mag, direction = self.get_ray(atom, neighbor)
+
                 # If atom and neighbor are different and their radii overlap, add neighbor to neighbors and add the
                 # plane to the edge list of each atom
                 if i != j and mag < atom.rad + neighbor.rad:
+
+                    # Define the planes radius by pythagorean theorem
+                    plane_radius = np.sqrt(atom.rad ** 2 - mag/2)
+
                     # Place the plane halfway between the atoms and set it's normal direction to the direction of the
-                    # ray between the atoms
-                    my_plane = [[atom.loc + direction*mag/2], [direction]]
+                    # ray between the atoms.
+                    my_plane = [[atom.loc[0] + direction[0]*mag/2,
+                                 atom.loc[1] + direction[1] * mag / 2,
+                                 atom.loc[2] + direction[2] * mag / 2,
+                                 ], [direction[0]*plane_radius,
+                                     direction[1]*plane_radius,
+                                     direction[2]*plane_radius]]
                     atom.edges.append(my_plane)
                     atom.neighbors.append(j)
-
-            #
-            for neighbor in atom.neighbors:
-
-                atom.shape = []
 
 
 atom0 = Atom(2, (1, 1, 1))
